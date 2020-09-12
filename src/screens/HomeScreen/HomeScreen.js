8c09bb500eefc6652efd51da639c9fe58b626438
probeta <33081197+ProBeta1@users.noreply.github.com>
2020-09-12 10:56:56 +0530

    boilerplate for login

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen(props) {
+    return (
+        <View>
+            <Text>Home </Text>
+        </View>
+    )
+}
