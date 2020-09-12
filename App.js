48edcc6c67bf60a37314163848e00348a5727b34
travischantravis <chancheukyin4463@gmail.com>
2020-09-12 12:20:43 -0700

    screen not blank

--- App.js
+++ App.js
@@ -1,43 +1,50 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen, AddItemScreen } from './src/screens'
-import { decode, encode } from 'base-64'
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  LoginScreen,
+  HomeScreen,
+  RegistrationScreen,
+  AddItemScreen,
+} from "./src/screens";
+import { decode, encode } from "base-64";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
-import { firebase } from './src/firebase/config'
-import SearchScreen from './src/screens/SearchScreen/SearchScreen';
-if (!global.btoa) { global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import { firebase } from "./src/firebase/config";
+import SearchScreen from "./src/screens/SearchScreen/SearchScreen";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const usersRef = firebase.firestore().collection('users');
-    firebase.auth().onAuthStateChanged(user => {
+    const usersRef = firebase.firestore().collection("users");
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
-            const userData = document.data()
-            setLoading(false)
-            setUser(userData)
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
           })
           .catch((error) => {
-            setLoading(false)
+            setLoading(false);
           });
       } else {
-        setLoading(false)
+        setLoading(false);
       }
     });
   }, []);
-
 
   return (
     <NavigationContainer>
@@ -45,36 +52,36 @@ const App = () => {
         {user ? (
           <>
             <Stack.Screen name="Home">
-              {props => <HomeScreen {...props} extraData={user} />}
+              {(props) => <HomeScreen {...props} extraData={user} />}
             </Stack.Screen>
             <Stack.Screen name="AddItem">
-              {props => <AddItemScreen {...props} extraData={user} />}
+              {(props) => <AddItemScreen {...props} extraData={user} />}
             </Stack.Screen>
             <Stack.Screen name="BuyItem">
-              {props => <SearchScreen {...props} extraData={user} />}
+              {(props) => <SearchScreen {...props} extraData={user} />}
             </Stack.Screen>
           </>
         ) : (
-            <>
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Registration" component={RegistrationScreen} />
-            </>
-          )}
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
   },
   horizontal: {
     flexDirection: "row",
     justifyContent: "space-around",
-    padding: 5
-  }
+    padding: 5,
+  },
 });
 
 export default App;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,30 +1,43 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import styles from '../HomeScreen/styles'
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../HomeScreen/styles";
 
 export default function HomeScreen(props) {
+  const user = props.extraData;
 
-    const user = props.extraData;
+  const onAddLinkPress = () => {
+    props.navigation.navigate("AddItem");
+  };
 
-    const onAddLinkPress = () => {
-        props.navigation.navigate('AddItem')
-    }
+  const onBuyLinkPress = () => {
+    props.navigation.navigate("BuyItem");
+  };
 
-    const onBuyLinkPress = () => { props.navigation.navigate('BuyItem') }
+  return (
+    <View>
+      <View style={styles.headerView}>
+        <Text style={styles.text}>
+          Hey {user.fullName} , how are you today ?
+        </Text>
+      </View>
 
-    return (
-        <View>
-            <View style={styles.headerView}>
-                <Text style={styles.text}>Hey {user.fullName} , how are you today ?</Text>
-            </View>
+      <View style={styles.footerView}>
+        <Text style={styles.footerText}>
+          Got stuff to sell ?{" "}
+          <Text onPress={onAddLinkPress} style={styles.footerLink}>
+            Add Here
+          </Text>
+        </Text>
+      </View>
 
-            <View style={styles.footerView}>
-                <Text style={styles.footerText}>Got stuff to sell ? <Text onPress={onAddLinkPress} style={styles.footerLink}>Add Here</Text></Text>
-            </View>
-
-            <View style={styles.footerView}>
-                <Text style={styles.footerText}>Wanna buy something ? <Text onPress={onBuyLinkPress} style={styles.footerLink}>Head over Here</Text></Text>
-            </View>
-        </View>
-    )
+      <View style={styles.footerView}>
+        <Text style={styles.footerText}>
+          Wanna buy something ?{" "}
+          <Text onPress={onBuyLinkPress} style={styles.footerLink}>
+            Head over Here
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
 }
