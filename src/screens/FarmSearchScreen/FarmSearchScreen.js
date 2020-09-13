29645fd3dae9bb21ac2b3f9e49a25dec081c4320
travischantravis <chancheukyin4463@gmail.com>
2020-09-12 20:40:50 -0700

    map with markers

--- src/screens/FarmSearchScreen/FarmSearchScreen.js
+++ src/screens/FarmSearchScreen/FarmSearchScreen.js
@@ -6,27 +6,49 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import axios from "axios";
 import { Ionicons } from "@expo/vector-icons";
+import MapView, { Marker } from "react-native-maps";
 
 // cropify project
 const GCP_API_KEY = "AIzaSyDg3QleDVQIZMBzAHylCxsPaZLf1eSQXSE";
 
 const FarmSearchScreen = () => {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Pune");
   const [farms, setFarms] = useState(null);
+  const [coords, setCoords] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 18.5169498,
+    longitude: 73.8340955,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
 
   const onPress = () => {
     // Google Places API
     axios
       .get(
-        `https://maps.googleapis.com/maps/api/place/textsearch/json?query=farms+India+${location}&radius=30000&key=${GCP_API_KEY}
+        `https://maps.googleapis.com/maps/api/place/textsearch/json?query=farms+India+${location}&radius=20000&key=${GCP_API_KEY}
     `
       )
       .then((response) => {
         const raw = response.data.results;
         setFarms(raw);
+        let coordsList = new Array();
+        console.log(raw[1]);
+        raw.forEach((data) => {
+          const latLng = data.geometry.location;
+          const newLatLng = { latitude: latLng.lat, longitude: latLng.lng };
+          coordsList.push({
+            latlng: newLatLng,
+            title: data.name,
+            rating: data.rating,
+          });
+        });
+        console.log(coordsList);
+        setCoords(coordsList);
       })
       .catch((err) => {
         console.log(err);
@@ -46,7 +68,7 @@ const FarmSearchScreen = () => {
           <Ionicons name="ios-search" size={32} color="black" />
         </TouchableOpacity>
       </View>
-      <View>
+      {/* <View>
         {farms &&
           farms.map((farm, i) => {
             return (
@@ -57,6 +79,30 @@ const FarmSearchScreen = () => {
               </View>
             );
           })}
+      </View> */}
+      <View>
+        <MapView
+          style={{ width: Dimensions.get("window").width, height: 300 }}
+          region={region}
+          // maxZoomLevel={4}
+          // initialRegion={{
+          //   latitude: 12.8677572,
+          //   longitude: 74.899232,
+          //   latitudeDelta: 2,
+          //   longitudeDelta: 0.0421,
+          // }}
+          provider="google"
+        >
+          {coords &&
+            coords.map((coord, i) => (
+              <Marker
+                key={i}
+                coordinate={coord.latlng}
+                title={coord.title}
+                description={`Rating: ${coord.rating.toString()}/5`}
+              />
+            ))}
+        </MapView>
       </View>
     </ScrollView>
   );
